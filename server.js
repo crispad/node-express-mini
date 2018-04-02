09fc211347b282b458b9190457d4df384b5a5274
Cristina Laryea <crispad06@gmail.com>
2018-04-02 16:24:52 -0400

    api users displaying on browser

--- server.js
+++ server.js
@@ -1,9 +1,38 @@
 const express = require('express');
+const db = require('./data/db.js');
 
 const server = express();
 
 server.get('/', function(req, res) {
     res.json({ api: 'Running...'});
+});
+
+server.get('/api/users', (req, res) => {
+    //get the data
+    //send the data
+    //send the error if there is one 
+    db
+        .find()
+        .then(users => {
+            res.json(users);
+        })
+        .catch(error => {
+            //handle it
+            res.status(500).json(error);
+        });
+});
+
+server.get('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+
+    db
+        .findById(id)
+        .then(users => {
+            res.json(users[0]);
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        });
 });
 
 const port = 5000;
