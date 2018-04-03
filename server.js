a58bf3c75f26113b96c905969155e76763c77d11
Cristina Laryea <crispad06@gmail.com>
2018-04-03 13:49:24 -0400

    added get and delete

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const morgan = require('morgan');
+const helmet = require('helmet');
+
 const db = require('./data/db.js');
+
 
 const server = express();
 
 //middleware 
 server.use(morgan('dev'));
+server.use(helmet());
 
 server.get('/', function(req, res) {
     res.json({ api: 'Running...'});
@@ -38,6 +42,64 @@ server.get('/api/users/:id', (req, res) => {
             res.status(500).json(error);
         });
 });
+
+server.post('/api/users', function(req, res) {
+    const user = req.body;
+    db
+        .insert(user)
+        .then(response => {
+            res.status(201).json(response);
+        })
+        .catch(error => {
+            res.status(500).json({
+                error: 'There was an error while saving the user to the database',
+            });
+        });
+});
+
+server.delete('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    let user;
+
+    db
+        .findById(id)
+        .then(response => {
+            user = {...response[0] };
+    db
+        .remove(id)
+        .then(response => {
+            res.status(200).json(user);
+        })
+        .catch(error => {
+            res.status(500).json(error);
+        });
+    })
+        .catch(error => {
+            res.status(500).json(error);
+        });
+});
+
+server.put('/api/users/:id', (req, res) => {
+    const { id } = req.params;
+    const update = req.body;
+  
+    db
+      .update(id, update)
+      .then(count => {
+        if (count > 0) {
+          db.findById(id).then(updatedUsers => {
+            res.status(200).json(updatedUsers[0]);
+          });
+        } else {
+          res
+            .status(404)
+            .json({ message: 'The user with the specified ID does not exist.' });
+        }
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  });
 
 const port = 5000;
 server.listen(port, () => console.log('API Running on port 5000'));
