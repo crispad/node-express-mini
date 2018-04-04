7189b81ec17d917be372bce8fc50e90441622e01
Cristina Laryea <crispad06@gmail.com>
2018-04-04 13:18:33 -0400

    creater folder, and file for router

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const express = require('express');
 //const morgan = require('morgan');
 const helmet = require('helmet');
 
-const db = require('./data/db.js');
-
+const userRouter = require('./users/userRouter.js');
 
 const server = express();
 
@@ -19,99 +18,18 @@ function logger(req, res, next) {
 }
 
 //middleware 
-server.use(morgan('dev'));
+//server.use(morgan('dev'));
 server.use(helmet());
 server.use(express.json());
+server.use(logger);
+
+server.use('/api/users', userRouter);
 
 server.get('/', function(req, res) {
     res.json({ api: 'Running...'});
 });
 
-server.get('/api/users', (req, res) => {
-    //get the data
-    //send the data
-    //send the error if there is one 
-    db
-        .find()
-        .then(users => {
-            res.json(users);
-        })
-        .catch(error => {
-            //handle it
-            res.status(500).json(error);
-        });
-});
 
-server.get('/api/users/:id', (req, res) => {
-    const { id } = req.params;
-
-    db
-        .findById(id)
-        .then(users => {
-            res.json(users[0]);
-        })
-        .catch(error => {
-            res.status(500).json(error);
-        });
-});
-
-server.post('/api/users', function(req, res) {
-    const user = req.body;
-    db
-        .insert(user)
-        .then(response => {
-            res.status(201).json(response);
-        })
-        .catch(error => {
-            res.status(500).json({
-                error: 'There was an error while saving the user to the database',
-            });
-        });
-});
-
-server.delete('/api/users/:id', (req, res) => {
-    const { id } = req.params;
-    let user;
-
-    db
-        .findById(id)
-        .then(response => {
-            user = {...response[0] };
-    db
-        .remove(id)
-        .then(response => {
-            res.status(200).json(user);
-        })
-        .catch(error => {
-            res.status(500).json(error);
-        });
-    })
-        .catch(error => {
-            res.status(500).json(error);
-        });
-});
-
-server.put('/api/users/:id', (req, res) => {
-    const { id } = req.params;
-    const update = req.body;
-  
-    db
-      .update(id, update)
-      .then(count => {
-        if (count > 0) {
-          db.findById(id).then(updatedUsers => {
-            res.status(200).json(updatedUsers[0]);
-          });
-        } else {
-          res
-            .status(404)
-            .json({ message: 'The user with the specified ID does not exist.' });
-        }
-      })
-      .catch(error => {
-        res.status(500).json(error);
-      });
-  });
 
 const port = 5000;
 server.listen(port, () => console.log('API Running on port 5000'));
